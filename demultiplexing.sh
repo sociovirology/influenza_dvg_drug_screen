b571c6b1d454cfb928918ee7f7d97cd543d5f5f4
Sam Díaz-Muñoz <sociovirology@Sams-Laptop.local>
2026-08-27 13:45:34 -0700

    Edits to file now that it only has demultiplexing commands for information.

--- demultiplexing.sh
+++ demultiplexing.sh
@@ -1,43 +1,36 @@
 #!/bin/bash
 
-# Basecalling and Demultiplexing Script for Influenza DVG Drug Screen
+# Demultiplexing Script for Influenza DelVG Drug Screen
 # Script: demultiplexing.sh
-# This file is shell script that downloads and processes raw sequencing reads
-# Requires three arguments that should be passed from the command line:
-#    run information, cross information, and experiment folders
+# This file is shell script that takes FASTQ's basecalled by Guppy (see basecalling.sh )  ONT (Oxford Nanopore Technologies) sequencing run folder output and basecalls FAST5 files to generate FASTQ files that downloads and processes raw sequencing reads
+ #In practive this file is not run by users as the demultiplexed FASTQ's are downloadable from ENA/SRA (SEE FILENAME)
+ #This file is provided here to provide insight into how the demultiplexed FASTQ's were generated
+
+# Requires entire ONT output directory and basecalling.sh to be run first.
 
 #This script is part of the following manuscript:
-#Influenza 
-#Ilechukwu Agu  | Ivy José |  Samuel L. Díaz Muñoz
+#Influenza A deletion-containing viral genome production is altered by metabolites, metabolic signaling molecules, and cyanobacterial extracts 
+#Ilechukwu Agu | Suzanna Y. Gomez | Ivy R. José | Samuel L. Díaz-Muñoz
 
-#Verify we have 3 arguments
 
-if [ "$#" -ne 3 ]; then
-    echo "You must enter exactly 3 command line arguments: run, cross list, and experiment"
-    echo "run has format 'runA'"
-    echo "File name for cross list. Cross list file is just a plain text file one line per cross: cross1 [break] cross9 etc. "
-    echo "Experiment can be any folder name you desire, e.g. 'infection_conditions'"
-    exit
-fi
+#Demultiplexing commands 
 
-#Checking command line parameters:
-echo "Checking command line arguments: run, cross list, and experiment"
-echo $1
-echo $2
-echo $3
+#Require both barcodes minimum scores F: 60, R: 40 
 
-#Download FASTQ sequencing files from SRA via EBI (Illumnia PE read files)
-#wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR218/043/SRR21880043/SRR21880043_1.fastq.gz -O data/runA_R1.fastq.gz
-#wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR218/043/SRR21880043/SRR21880043_2.fastq.gz -O data/runA_R2.fastq.gz
+~/ont-guppy/bin/guppy_barcoder --require_barcodes_both_ends --input_path /mnt/data0/sam/dvg_drug_screen_ile/fastq_hac/pass --save_path /mnt/data0/sam/dvg_drug_screen_ile/fastq_hac/pass/double_barcodes_60_40/ --detect_mid_strand_barcodes --barcode_kits EXP-PBC096 --min_score_barcode_rear 40 --min_score_barcode_front 60 -x cuda:all:100% --worker_threads 32
+#ONT Guppy barcoding software version 6.5.7+ca6d6af
+#input path:         /mnt/data0/sam/dvg_drug_screen_ile/fastq_hac/pass
+#save path:          /mnt/data0/sam/dvg_drug_screen_ile/fastq_hac/pass/double_barcodes_60_40/
+#min. score front:   60
+#min. score rear:    40
+#gpu device:         cuda:all:100%
 
-#Unzip files
-#gunzip data/runA_*.fastq.gz
+#Found 5324 input files.
 
-#First let's basecall
-~/ont-guppy/bin/guppy_basecaller --input_path '/media/user/Lab Portab/dvg_dug_screen_ile_v2/no_sample/20230921_1151_MN23913_FAW91824_5830ce8e/' --save_path /mnt/data0/sam/dvg_drug_screen_ile/fastq_hac/ -c dna_r10.4.1_e8.2_400bps_hac.cfg -x cuda:all:100% --chunks_per_runner 1024 --recursive --calib_detect --do_read_splitting
-
-#Then let's demultiplex
-~/ont-guppy/bin/guppy_barcoder --require_barcodes_both_ends --input_path /mnt/data0/sam/dvg_drug_screen_ile/fastq_hac/pass/ --save_path /mnt/data0/sam/dvg_drug_screen_ile/fastq_hac/pass/double_barcodes/ --detect_mid_strand_barcodes --barcode_kits EXP-PBC096 -x cuda:all:100% --worker_threads 16 
+#0%   10   20   30   40   50   60   70   80   90   100%
+#|----|----|----|----|----|----|----|----|----|----|
+#***************************************************
+#Done in 18009466 ms.
 
 #Let us know it's done
 echo "Finished!"
